e23dfd8b4a3677e8be815a38562c8f1ca0c1ecca
Kazato Sugimoto <uiureo@gmail.com>
2022-07-18 21:37:05 +0900

    Implement builder/tests/06-optional-field.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,25 +8,42 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
     let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
-    let builder_error_name = Ident::new(&format!("{}BuilderError", name), Span::call_site());
     let mut field_key: Vec<Ident> = vec![];
     let mut field_type: Vec<Type> = vec![];
 
-    match input.data {
-        syn::Data::Struct(s) => match s.fields {
-            syn::Fields::Named(n) => {
-                for field in n.named.iter() {
-                    if let Some(field_ident) = field.ident.clone() {
-                        field_key.push(field_ident);
+    let mut optional_field_key: Vec<Ident> = vec![];
+    let mut optional_field_type: Vec<Type> = vec![];
+
+    if let syn::Data::Struct(s) = input.data {
+        if let syn::Fields::Named(n) = s.fields {
+            for field in n.named.iter() {
+                if let Some(field_ident) = field.ident.clone() {
+                    let mut is_option = false;
+                    if let Type::Path(t) = &field.ty {
+                        let result = t
+                            .path
+                            .segments
+                            .iter()
+                            .find(|segment| segment.ident == "Option");
+
+                        if let Some(segment) = result {
+                            if let syn::PathArguments::AngleBracketed(t) = &segment.arguments {
+                                if let Some(syn::GenericArgument::Type(ty)) = t.args.first() {
+                                    is_option = true;
+                                    optional_field_key.push(field_ident.clone());
+                                    optional_field_type.push(ty.clone());
+                                }
+                            }
+                        }
+                    }
+
+                    if !is_option {
+                        field_key.push(field_ident.clone());
                         field_type.push(field.ty.clone());
                     }
                 }
             }
-            syn::Fields::Unnamed(_) => todo!(),
-            syn::Fields::Unit => todo!(),
-        },
-        syn::Data::Enum(_) => todo!(),
-        syn::Data::Union(_) => todo!(),
+        }
     }
 
     let expanded = quote! {
@@ -34,15 +51,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #(
                 #field_key: Option<#field_type>,
             )*
+            #(
+                #optional_field_key: Option<#optional_field_type>,
+            )*
         }
-        #[derive(std::fmt::Debug)]
-        struct #builder_error_name;
-        impl std::fmt::Display for #builder_error_name {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "error!")
-            }
-        }
-        impl std::error::Error for #builder_error_name {}
 
         impl #builder_name {
             #(
@@ -51,15 +63,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     self
                 }
             )*
+            #(
+                fn #optional_field_key(&mut self, #optional_field_key: #optional_field_type) -> &mut #builder_name {
+                    self.#optional_field_key = Some(#optional_field_key);
+                    self
+                }
+            )*
 
             fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
                 #(
-                    if let None = self.#field_key.as_ref() {
+                    if self.#field_key.is_none() {
                         return Err(format!("{} is missing", stringify!(#field_key)).into());
                     }
                 )*
                 Ok(#name {
                     #(#field_key: self.#field_key.clone().unwrap(),)*
+                    #(#optional_field_key: self.#optional_field_key.clone(),)*
                 })
             }
         }
@@ -69,6 +88,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 #builder_name {
                     #(
                         #field_key: None,
+                    )*
+
+                    #(
+                        #optional_field_key: None,
                     )*
                 }
             }
