76fb067f00e8cde59eb87fd01288bdd55f1633b4
Kazato Sugimoto <uiureo@gmail.com>
2022-07-20 21:40:06 +0900

    Implement debug/tests/01-parse.rs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let expanded = quote! {};
+    expanded.into()
 }
