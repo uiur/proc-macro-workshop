8d7ed2280a0a83d0c725b682227f4bb038db11fe
Kazato Sugimoto <uiureo@gmail.com>
2022-07-20 22:08:52 +0900

    Implement debug/tests/02-impl-debug.rs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,38 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, FieldsNamed};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let expanded = quote! {};
-    expanded.into()
+    let ident = &input.ident;
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = &input.data
+    {
+        let parsed_fields = named
+            .iter()
+            .cloned()
+            .filter_map(|field| field.ident.map(|ident| (ident, field.ty)))
+            .collect::<Vec<_>>();
+
+        let (field_idents, field_tys): (Vec<_>, Vec<_>) = parsed_fields.into_iter().unzip();
+
+        println!("{:#?}", input);
+        let expanded = quote! {
+            impl std::fmt::Debug for #ident {
+                fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                    fmt.debug_struct(stringify!(#ident))
+                        #(
+                            .field(stringify!(#field_idents), &self.#field_idents)
+                        )*
+                        .finish()
+                }
+            }
+        };
+        expanded.into()
+    } else {
+        TokenStream::new()
+    }
 }
